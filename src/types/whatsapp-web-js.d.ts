9f522c67fb2c849f0db16be936c7430e574bcabe
LDanielOchoa <daniel.ochoaa.cass@gmail.com>
2025-09-08 09:27:48 -0500

    Add: bug de dependencia faltante

--- src/types/whatsapp-web-js.d.ts
+++ src/types/whatsapp-web-js.d.ts
@@ -1,1 +1,13 @@
-declare module 'whatsapp-web.js';
+declare module 'whatsapp-web.js' {
+  export class Client {
+    constructor(options?: any);
+    // Add any other methods/properties you use if you want better type checking
+  }
+  export class LocalAuth {
+    constructor(options?: any);
+  }
+  export class MessageMedia {
+    constructor(mimetype: string, data: string, filename?: string);
+    static fromUrl(url: string, options?: any): Promise<MessageMedia>;
+  }
+}
